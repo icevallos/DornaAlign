0c7f4fa13f4392f975e20b3e4701aa4848a0ce2a
icevallos <i.cevallos@columbia.edu>
2021-08-16 12:39:00 -0400

    laser serial fix to connection,string readout

--- laser_serial.py
+++ laser_serial.py
@@ -7,9 +7,11 @@ import io
 class LaserIO(object):
 
 
-    def __init__(port = "dev/ttyUSB0"):
+    def __init__(self, port_name = "/dev/ttyUSB0"):
 
-        self.ser = serial.Serial(port,9600,timeout = 1)
+
+        self.port_name = port_name
+        self.ser = serial.Serial(self.port_name,9600,timeout = 1)
 
         self.ser_io = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser, 1),  
                                newline = '\r',
@@ -35,16 +37,28 @@ class LaserIO(object):
         if i == -1:
             print("M0 measurement not found")
             return None
-        j = out[i:].find[","]
+        j = out[i:].find(",")
 
         head_val =  out[i:i+j]
+        print(head_val)
 
-        if head_val.isnumeric():
+        if self.isfloat(head_val):
             distance  = float(head_val)
             return distance 
         else:
             print("Laser head out of range")
             return None
+
+    def isfloat(self,value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+
+    def close(self):
+        self.ser.close()
+        return None
 
 
         
